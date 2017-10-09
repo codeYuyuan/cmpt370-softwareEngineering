cf6f89f3fbeb250c02d1d7c406acb187f7aa1591
codeYuyuan <yuyuan.liu@usask.ca>
2017-10-09 17:57:20 -0600

    implemented user authentication

--- app.js
+++ app.js
@@ -4,28 +4,65 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session  = require('express-session');
+var morgan = require('morgan');
 
+var passport = require('passport');
+var flash    = require('connect-flash');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var pets = require('./routes/pets');
 
 var app = express();
 
+
+// connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use(require("express-session")({
+    secret:"Secret session",
+    resave: false,
+    saveUninitialized:false
+}));
+// app.use(session({
+//     secret: 'vidyapathaisalwaysrunning',
+//     resave: true,
+//     saveUninitialized: true
+// } )); // session secret
+app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+app.use(function(req,res,next){
+    res.locals.user = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
+
 app.use('/users', users);
 app.use('/pets',pets);
+require('./routes/index.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,95 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(app, passport) {
+    /* GET home page. */
+    app.get('/', function(req, res, next) {
+        res.render('mainPage');
+    });
+    app.get('/about', function(req, res, next) {
+        res.render('about')
+    });
+    app.get('/search', function(req, res, next) {
+        res.render('search')
+    });
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('mainPage.ejs'); // load the index.ejs file
+    });
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('mainPage');
-});
-router.get('/about', function(req, res, next) {
-    res.render('about')
-});
-router.get('/search', function(req, res, next) {
-    res.render('search')
-});
-router.get('/login', function (req,res, next) {
-    res.render('login');
-})
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
 
-router.get('/register', function (req,res, next) {
-    res.render('register');
-})
-module.exports = router;
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs');
+    });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            successFlash: 'Log in successfully!',
+            failureFlash : 'Invalid username or password.' // allow flash messages
+        }),
+        function(req, res) {
+            console.log("hello");
+
+            if (req.body.remember) {
+                req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+                req.session.cookie.expires = false;
+            }
+            res.redirect('/');
+        });
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/register', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('register.ejs');
+    });
+
+    // process the signup form
+    app.post('/register', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/register', // redirect back to the signup page if there is an error
+        successFlash: 'Create account successfully!',
+        failureFlash : 'Empty field or username already exits.' // allow flash messages
+    }));
+
+    // =====================================
+    // PROFILE SECTION =========================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
